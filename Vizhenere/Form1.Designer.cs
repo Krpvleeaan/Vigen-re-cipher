b40e10e175837c6a4c7860405f7b92f243169ba0
Krpvleeaan <84187515+Krpvleeaan@users.noreply.github.com>
2022-03-21 21:56:29 +0300

    Add files via upload
    Vigenère cipher - a method of polyalphabetic encryption of literal text

---
+++ Vizhenere/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace Vizhenere
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 55);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(274, 159);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(457, 55);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(286, 159);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 275);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(274, 151);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Зашифровать",
+            "Расшифровать"});
+            this.comboBox1.Location = new System.Drawing.Point(412, 301);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Russian",
+            "English"});
+            this.comboBox2.Location = new System.Drawing.Point(412, 398);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(619, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(506, 13);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(192, 36);
+            this.richTextBox4.TabIndex = 6;
+            this.richTextBox4.Text = "Ключ";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(76, 13);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.ReadOnly = true;
+            this.richTextBox5.Size = new System.Drawing.Size(151, 37);
+            this.richTextBox5.TabIndex = 7;
+            this.richTextBox5.Text = "Введите текст";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(47, 220);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.ReadOnly = true;
+            this.richTextBox6.Size = new System.Drawing.Size(192, 36);
+            this.richTextBox6.TabIndex = 8;
+            this.richTextBox6.Text = "Расшифрованный текст";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(412, 245);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.ReadOnly = true;
+            this.richTextBox7.Size = new System.Drawing.Size(151, 37);
+            this.richTextBox7.TabIndex = 9;
+            this.richTextBox7.Text = "Выберите действие";
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.Location = new System.Drawing.Point(412, 348);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.ReadOnly = true;
+            this.richTextBox8.Size = new System.Drawing.Size(151, 37);
+            this.richTextBox8.TabIndex = 10;
+            this.richTextBox8.Text = "Выберите язык";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+    }
+}
+
---
+++ Vizhenere/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vizhenere
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem != "Russian" && comboBox2.SelectedItem != "English" && comboBox1.SelectedItem != "Зашифровать" && comboBox1.SelectedItem != "Расшифровать")
+            {
+                richTextBox3.Text = "";
+                MessageBox.Show("Выберите что нибудь");
+            }
+            if ((comboBox2.SelectedItem == "Russian" || comboBox2.SelectedItem == "English") && comboBox1.SelectedItem != "Зашифровать" && comboBox1.SelectedItem != "Расшифровать")
+            {
+                richTextBox3.Text = "";
+                MessageBox.Show("Выберите , что нужно сделать с текстом");
+            }
+            if (comboBox2.SelectedItem != "Russian" && comboBox2.SelectedItem != "English" && (comboBox1.SelectedItem == "Зашифровать" || comboBox1.SelectedItem == "Расшифровать"))
+            {
+                richTextBox3.Text = "";
+                MessageBox.Show("Выберите язык");
+            }
+            if (comboBox2.SelectedItem == "Russian" && comboBox1.SelectedItem == "Зашифровать")
+            {
+                if (Checker_For_Russian() == 1)
+                {
+                    richTextBox3.Text = "";
+                    richTextBox3.AppendText(EncryptionRU());
+                }
+            }
+            if (comboBox2.SelectedItem == "Russian" && comboBox1.SelectedItem == "Расшифровать")
+            {
+                if (Checker_For_Russian() == 1)
+                {
+                    richTextBox3.Text = "";
+                    richTextBox3.AppendText(DecryptionRU());
+                }
+            }
+            if (comboBox2.SelectedItem == "English" && comboBox1.SelectedItem == "Зашифровать")
+            {
+                if (Checker_For_English() == 1)
+                {
+                    richTextBox3.Text = "";
+                    richTextBox3.AppendText(EncryptionENG());
+                }
+            }
+            if (comboBox2.SelectedItem == "English" && comboBox1.SelectedItem == "Расшифровать")
+            {
+                if (Checker_For_English() == 1)
+                {
+                    richTextBox3.Text = "";
+                    richTextBox3.AppendText(DecryptionENG());
+                }
+            }
+        }
+        public string Key_plus_ru(string original_text, string key)
+        {
+            string Alpha = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789";
+            int check = 0;
+            if (key == "")
+                return (null);
+            for (int i = 0; i < key.Length; i++)
+            {
+                for (int j = 0; j < Alpha.Length; j++)
+                {
+                    if (key[i] == Alpha[j])
+                    {
+                        check = 0;
+                        break;
+                    }
+                    check = 1;
+                }
+                if (check == 1)
+                    return (null);
+            }
+            int length = key.Length;
+            if (original_text.Length > length)
+                for (int i = 0; i < original_text.Length - length; i++)
+                    key += key[i];
+            return key;
+        }
+        public string Key_plus_eng(string original_text, string key)
+        {
+            string Alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
+            int check = 0;
+            if (key == "")
+                return (null);
+            for (int i = 0; i < key.Length; i++)
+            {
+                for (int j = 0; j < Alpha.Length; j++)
+                {
+                    if (key[i] == Alpha[j])
+                    {
+                        check = 0;
+                        break;
+                    }
+                    check = 1;
+                }
+                if (check == 1)
+                    return (null);
+            }
+            int length = key.Length;
+            if (original_text.Length > length)
+                for (int i = 0; i < original_text.Length - length; i++)
+                    key += key[i];
+            return key;
+        }
+        public string EncryptionRU()
+        {
+            string FINAL = "";
+            string ru_Alpha = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789";
+            string original_text = richTextBox1.Text.ToLower();
+            original_text = original_text.Replace(" ", "");
+            string key = Key_plus_ru(original_text, richTextBox2.Text.ToLower());
+            if (key == null)
+            {
+                MessageBox.Show("Неверный ключ");
+                return ("");
+            }    
+            int[] arr = new int[original_text.Length];
+            for (int i = 0; i < original_text.Length; i++)
+            {
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (ru_Alpha[j] == key[i])
+                        arr[i] = j;
+                }
+            }
+            for (int i = 0; i < original_text.Length; i++)
+            {
+                if (original_text[i] == ' ')
+                    continue;
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (ru_Alpha[j] == original_text[i])
+                    {
+                        FINAL += ru_Alpha[(ru_Alpha.Length + j + arr[i]) % 43];
+                        break;
+                    }
+                }
+            }
+            return (FINAL);
+        }
+        public string DecryptionRU()
+        {
+            string FINAL = "";
+            string ru_Alpha = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789";
+            string original_text = richTextBox1.Text.ToLower();
+            original_text = original_text.Replace(" ", "");
+            string key = Key_plus_ru(original_text, richTextBox2.Text.ToLower());
+            if (key == null)
+            {
+                MessageBox.Show("Неверный ключ");
+                return ("");
+            }
+            int[] arr = new int[original_text.Length];
+            for (int i = 0; i < original_text.Length; i++)
+            {
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (ru_Alpha[j] == key[i])
+                        arr[i] = j;
+                }
+            }
+            for (int i = 0; i < original_text.Length; i++)
+            {
+                if (original_text[i] == ' ')
+                    continue;
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (ru_Alpha[j] == original_text[i])
+                    {
+                        FINAL += ru_Alpha[(ru_Alpha.Length + j - arr[i]) % 43];
+                        break;
+                    }
+                }
+            }
+            return (FINAL);
+        }
+        public string EncryptionENG()
+        {
+            string FINAL = "";
+            string ru_Alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
+            string original_text = richTextBox1.Text.ToLower();
+            original_text = original_text.Replace(" ", "");
+            string key = Key_plus_eng(original_text, richTextBox2.Text.ToLower());
+            if (key == null)
+            {
+                MessageBox.Show("Неверный ключ");
+                return ("");
+            }
+            int[] arr = new int[original_text.Length];
+            for (int i = 0; i < original_text.Length; i++)
+            {
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (ru_Alpha[j] == key[i])
+                        arr[i] = j;
+                }
+            }
+            for (int i = 0; i < original_text.Length; i++)
+            {
+                if (original_text[i] == ' ')
+                    continue;
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (ru_Alpha[j] == original_text[i])
+                    {
+                        FINAL += ru_Alpha[(ru_Alpha.Length + j + arr[i]) % 36];
+                        break;
+                    }
+                }
+            }
+            return (FINAL);
+        }
+        public string DecryptionENG()
+        {
+            string FINAL = "";
+            string ru_Alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
+            string original_text = richTextBox1.Text.ToLower();
+            original_text = original_text.Replace(" ", "");
+            string key = Key_plus_eng(original_text, richTextBox2.Text.ToLower());
+            if (key == null)
+            {
+                MessageBox.Show("Неверный ключ");
+                return ("");
+            }
+            int[] arr = new int[original_text.Length];
+            for (int i = 0; i < original_text.Length; i++)
+            {
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (ru_Alpha[j] == key[i])
+                        arr[i] = j;
+                }
+            }
+            for (int i = 0; i < original_text.Length; i++)
+            {
+                if (original_text[i] == ' ')
+                    continue;
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (ru_Alpha[j] == original_text[i])
+                    {
+                        FINAL += ru_Alpha[(ru_Alpha.Length + j - arr[i]) % 36];
+                        break;
+                    }
+                }
+            }
+            return (FINAL);
+        }
+        public int Checker_For_Russian()
+        {
+            string text = richTextBox1.Text;
+            if (text == "")
+            {
+                MessageBox.Show("Вы не ввели текст");
+                return (0);
+            }
+            string ru_Alpha = " абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789";
+            for (int i = 0; i < text.Length; i++)
+            {
+                int checker = 0;
+                for (int j = 0; j < ru_Alpha.Length; j++)
+                {
+                    if (text[i] == ru_Alpha[j])
+                        checker = 1;
+                }
+                if (checker == 0)
+                {
+                    MessageBox.Show("Вы ввели некорректные символы в тексте");
+                    return (0);
+                }
+            }
+            return (1);
+        }
+        public int Checker_For_English()
+        {
+            string text = richTextBox1.Text;
+            string eng_Alpha = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            if (text == "")
+            {
+                MessageBox.Show("Вы не ввели текст");
+                return (0);
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                int checker = 0;
+                for (int j = 0; j < eng_Alpha.Length; j++)
+                {
+                    if (text[i] == eng_Alpha[j])
+                        checker = 1;
+                }
+                if (checker == 0)
+                {
+                    MessageBox.Show("Вы ввели некорректные символы в тексте");
+                    return (0);
+                }
+            }
+            return (1);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
